535400508b9055cec4f961eed95249c98a94b796
Johannes Holzfuß <DataWraith@web.de>
2013-08-25 21:36:20 +0200

    Make the test pass

--- cgp.go
+++ cgp.go
@@ -5,8 +5,37 @@ type EvalFunction func(Individual) float64
 type RndConstFunction func() float64
 
 type CGP struct {
+	PopSize      uint
+	MaxGenes     uint
+	MutationRate float64
+	NumInputs    uint
+	NumOutputs   uint
+	MaxArity     uint
+	FunctionList []CGPFunction
+	RandConst    RndConstFunction
+	Evaluator    EvalFunction
+	Population   []Individual
 }
 
 func New(popSize uint, maxGenes uint, mutationRate float64, numInputs uint, numOutputs uint, maxArity uint, functionList []CGPFunction, randomConstant RndConstFunction, evaluator EvalFunction) *CGP {
-	return &CGP{}
+	result := &CGP{
+		PopSize:      popSize,
+		MaxGenes:     maxGenes,
+		MutationRate: mutationRate,
+		NumInputs:    numInputs,
+		NumOutputs:   numOutputs,
+		MaxArity:     maxArity,
+		FunctionList: functionList,
+		RandConst:    randomConstant,
+		Evaluator:    evaluator,
+	}
+
+	for i := uint(0); i < result.PopSize; i++ {
+		result.Population = append(result.Population, NewIndividual(result))
+	}
+
+	return result
+}
+
+func (cgp *CGP) RunGeneration() {
 }
--- individual.go
+++ individual.go
@@ -1,4 +1,13 @@
 package cgp
 
 type Individual struct {
+	Fitness float64
+}
+
+func NewIndividual(cgp *CGP) Individual {
+	return Individual{}
+}
+
+func (ind Individual) Run(input []float64) (output []float64) {
+	return
 }
