6eb1b961009bdc253e7b54892f5597233e6b90f4
Johannes Holzfuß <DataWraith@web.de>
2013-08-28 21:52:54 +0200

    Implement CGP.RunGeneration method

--- cgp.go
+++ cgp.go
@@ -1,5 +1,9 @@
 package cgp
 
+import (
+	"math"
+)
+
 type CGPFunction func([]float64) float64
 type EvalFunction func(Individual) float64
 type RndConstFunction func() float64
@@ -34,6 +38,7 @@ func New(popSize uint, maxGenes uint, mutationRate float64, numInputs uint, numO
 	if len(functionList) == 0 {
 		panic("At least one function must be provided.")
 	}
+
 	result := &CGP{
 		PopSize:      popSize,
 		MaxGenes:     maxGenes,
@@ -44,14 +49,38 @@ func New(popSize uint, maxGenes uint, mutationRate float64, numInputs uint, numO
 		FunctionList: functionList,
 		RandConst:    randomConstant,
 		Evaluator:    evaluator,
+		Population:   make([]Individual, 1, popSize),
 	}
 
-	for i := uint(0); i < result.PopSize; i++ {
-		result.Population = append(result.Population, NewIndividual(result))
-	}
+	result.Population[0] = NewIndividual(result)
 
 	return result
 }
 
 func (cgp *CGP) RunGeneration() {
+	// Create offspring
+	cgp.Population = cgp.Population[0:1]
+	for i := uint(1); i < cgp.PopSize; i++ {
+		cgp.Population = append(cgp.Population, cgp.Population[0].Mutate())
+	}
+
+	// Evaluate offspring
+	// TODO: Parallelize this
+	for i := 1; uint(i) < cgp.PopSize; i++ {
+		cgp.Population[i].Fitness = cgp.Evaluator(cgp.Population[i])
+	}
+
+	// Replace parent with best offspring
+	bestFitness := math.Inf(1)
+	bestIndividual := uint(0)
+	for i := uint(1); i < cgp.PopSize; i++ {
+		if cgp.Population[i].Fitness < bestFitness {
+			bestFitness = cgp.Population[i].Fitness
+			bestIndividual = i
+		}
+	}
+
+	if bestFitness <= cgp.Population[0].Fitness {
+		cgp.Population[0] = cgp.Population[bestIndividual]
+	}
 }
--- individual.go
+++ individual.go
@@ -1,13 +1,25 @@
 package cgp
 
+import (
+	"math"
+)
+
 type Individual struct {
+	Options *CGP
 	Fitness float64
 }
 
 func NewIndividual(cgp *CGP) Individual {
-	return Individual{}
+	return Individual{
+		Options: cgp,
+		Fitness: math.Inf(1),
+	}
+}
+
+func (ind Individual) Mutate() Individual {
+	return NewIndividual(ind.Options)
 }
 
 func (ind Individual) Run(input []float64) (output []float64) {
-	return
+	return []float64{0, 0, 0}
 }
