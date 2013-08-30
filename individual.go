af3b278a1fb75e0835da769b84b20b3b5d6b62fd
Johannes Holzfuß <DataWraith@web.de>
2013-08-30 21:18:44 +0200

    Write Individual.Mutate()

--- individual.go
+++ individual.go
@@ -11,8 +11,12 @@ type Gene struct {
 	Connections []int
 }
 
+func (g *Gene) Mutate(options *CGP) {
+}
+
 type Individual struct {
 	Genes   []Gene
+	Outputs []int
 	Options *CGP
 	Fitness float64
 }
@@ -21,6 +25,7 @@ func NewIndividual(cgp *CGP) (ind Individual) {
 	ind.Options = cgp
 	ind.Fitness = math.Inf(1)
 	ind.Genes = make([]Gene, cgp.NumGenes)
+	ind.Outputs = make([]int, cgp.NumOutputs)
 
 	for i := range ind.Genes {
 		ind.Genes[i].Function = rand.Intn(len(cgp.FunctionList))
@@ -31,11 +36,41 @@ func NewIndividual(cgp *CGP) (ind Individual) {
 		}
 	}
 
+	for i := range ind.Outputs {
+		ind.Outputs[i] = rand.Intn(cgp.NumInputs + cgp.NumGenes)
+	}
+
 	return
 }
 
-func (ind Individual) Mutate() Individual {
-	return NewIndividual(ind.Options)
+func (ind Individual) Mutate() (mutant Individual) {
+	// Copy the parent individual
+	mutant.Fitness = math.Inf(1)
+	mutant.Options = ind.Options
+	mutant.Genes = make([]Gene, ind.Options.NumGenes)
+	mutant.Outputs = make([]int, ind.Options.NumOutputs)
+	copy(mutant.Genes, ind.Genes)
+	copy(mutant.Outputs, ind.Outputs)
+
+	numMutations := ind.Options.MutationRate * float64(ind.Options.NumGenes+ind.Options.NumOutputs)
+	if numMutations < 1 {
+		numMutations = 1
+	}
+
+	for numMutations > 0 {
+		toMutate := rand.Intn(mutant.Options.NumGenes + mutant.Options.NumOutputs)
+
+		if toMutate < mutant.Options.NumGenes {
+			mutant.Genes[toMutate].Mutate(mutant.Options)
+		} else {
+			mutant.Outputs[toMutate-mutant.Options.NumGenes] =
+				rand.Intn(mutant.Options.NumInputs + mutant.Options.NumGenes)
+		}
+
+		numMutations--
+	}
+
+	return
 }
 
 func (ind Individual) Run(input []float64) (output []float64) {
